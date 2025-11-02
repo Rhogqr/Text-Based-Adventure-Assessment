37d89f0d44ff8563a3ea127f9df493d2a4cb8365
Rhogar <reuben@coreproductions.com.au>
2025-11-02 22:01:51 +1100

    Reformat the Class Array to have it use pointers, letting the subclasses methods and variables not be sliced off. Also adds implementation for the math puzzle room

--- Text Based Adventure Assessment/Rooms.h
+++ Text Based Adventure Assessment/Rooms.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <vector>
+#include <typeinfo>
 
 using namespace std;
+
+string EmptyRoomDescription = "This is an empty room. Your only option is to move.";
+string PuzzleRoomDescription = "This is a puzzle room. You have to solve this equation to continue.";
+string TeleportRoomDescription = "This is a teleport room. You will be teleported to a random room on the map.";
 
 class Room
 {
@@ -9,6 +16,7 @@ private:
 	int xCoord, yCoord;
 public:
 	bool doorNorth, doorSouth, doorEast, doorWest;
+	string description;
 	// constructors
 	Room()
 	{
@@ -18,9 +26,10 @@ public:
 		doorSouth = false;
 		doorEast = false;
 		doorWest = false;
+		description = "peepee";
 	}
  
-	Room(int x, int y, bool dN, bool dS, bool dE, bool dW)
+	Room(int x, int y, bool dN, bool dS, bool dE, bool dW, string desc)
 	{
 		xCoord = x;
 		yCoord = y;
@@ -28,6 +37,7 @@ public:
 		doorSouth = dS;
 		doorEast = dE;
 		doorWest = dW;
+		description = desc;
 	}
 	
 	char displayRoom(int x, int y)
@@ -42,42 +52,109 @@ public:
 			return ' ';
 		}
 	}
+
+	virtual void specialRoomFunction()
+	{
+		// base Room class has no special function, so does nothing
+	}
 };
 
 class EmptyRoom : public Room
 {
+public:
 	// uses the same constructors as Room
 	EmptyRoom() : Room() {}
-	EmptyRoom(int x, int y, bool dN, bool dS, bool dE, bool dW) : Room(x, y, dN, dS, dE, dW) {}
+	EmptyRoom(int x, int y, bool dN, bool dS, bool dE, bool dW, string desc) : Room(x, y, dN, dS, dE, dW, desc) { desc = EmptyRoomDescription; }
 
 	// same displayRoom function as Room
 	char displayRoom(int x, int y)
 	{
 		return Room::displayRoom(x, y);
 	}
+
+	void specialRoomFunction()
+	{
+		// empty room has no special function, so always does nothing
+	}
+};
+class PuzzleRoom : public Room
+{
+public:
+	// uses the same constructors as Room
+	PuzzleRoom() : Room() {}
+	PuzzleRoom(int x, int y, bool dN, bool dS, bool dE, bool dW, string desc) : Room(x, y, dN, dS, dE, dW, desc) { desc = PuzzleRoomDescription; }
+
+	// same displayRoom function as Room
+	char displayRoom(int x, int y)
+	{
+		return Room::displayRoom(x, y);
+	}
+
+	void specialRoomFunction()
+	{
+		while (true)
+		{
+			int rand1 = rand() % 100;
+			int rand2 = rand() % 100;
+			int answer;
+			cout << "What is " << rand1 << "+" << rand2 << endl;
+			cin >> answer;
+			if (answer == (rand1 + rand2))
+			{
+				cout << "Correct! You may proceed." << endl;
+				break;
+			}
+			else
+			{
+				cout << "Incorrect. Try again." << endl;
+			}
+		}
+	}
+};
+class TeleportRoom : public Room
+{
+public:
+	TeleportRoom() : Room() {}
+	TeleportRoom(int x, int y, bool dN, bool dS, bool dE, bool dW, string desc) : Room(x, y, dN, dS, dE, dW, desc) { desc = TeleportRoomDescription; }
+
+	char displayRoom(int x, int y)
+	{
+		return Room::displayRoom(x, y);
+	}
+
+	void specialRoomFunction()
+	{
+		int rand1 = rand() % 4;
+		int rand2 = rand() % 4;
+
+		cout << "You have been teleported to a random room!" << endl;
+	}
 };
 
-void displayMap(Room rooms[4][4], int playerX, int playerY)
+void displayMap(Room* rooms[4][4], int playerX, int playerY)
 {
-	// Clears the console by printing 50 new lines
-	for (int i = 0; i < 50; i++)
-	{
-		cout << endl;
-	}
-	// displays the map with the player's position marked by an 'X', and a space if the player is not in that room
-	cout << "-----------------" << endl;
-	cout << "| " << rooms[0][0].displayRoom(playerX, playerY) << " | " << rooms[1][0].displayRoom(playerX, playerY) << " | " << rooms[2][0].displayRoom(playerX, playerY) << " | " << rooms[3][0].displayRoom(playerX, playerY) << " |" << endl;
-	cout << "| " << rooms[0][1].displayRoom(playerX, playerY) << " | " << rooms[1][1].displayRoom(playerX, playerY) << " | " << rooms[2][1].displayRoom(playerX, playerY) << " | " << rooms[3][1].displayRoom(playerX, playerY) << " |" << endl;
-	cout << "| " << rooms[0][2].displayRoom(playerX, playerY) << " | " << rooms[1][2].displayRoom(playerX, playerY) << " | " << rooms[2][2].displayRoom(playerX, playerY) << " | " << rooms[3][2].displayRoom(playerX, playerY) << " |" << endl;
-	cout << "| " << rooms[0][3].displayRoom(playerX, playerY) << " | " << rooms[1][3].displayRoom(playerX, playerY) << " | " << rooms[2][3].displayRoom(playerX, playerY) << " | " << rooms[3][3].displayRoom(playerX, playerY) << " |" << endl;
-	cout << "-----------------" << endl;
+    // Clears the console by printing 50 new lines
+    for (int i = 0; i < 50; i++)
+    {
+        cout << endl;
+    }
+	
+    // displays the map with the player's position marked by an 'X', and a space if the player is not in that room
+    cout << "-----------------" << endl;
+    cout << "| " << rooms[0][0]->displayRoom(playerX, playerY) << " | " << rooms[1][0]->displayRoom(playerX, playerY) << " | " << rooms[2][0]->displayRoom(playerX, playerY) << " | " << rooms[3][0]->displayRoom(playerX, playerY) << " |" << endl;
+    cout << "| " << rooms[0][1]->displayRoom(playerX, playerY) << " | " << rooms[1][1]->displayRoom(playerX, playerY) << " | " << rooms[2][1]->displayRoom(playerX, playerY) << " | " << rooms[3][1]->displayRoom(playerX, playerY) << " |" << endl;
+    cout << "| " << rooms[0][2]->displayRoom(playerX, playerY) << " | " << rooms[1][2]->displayRoom(playerX, playerY) << " | " << rooms[2][2]->displayRoom(playerX, playerY) << " | " << rooms[3][2]->displayRoom(playerX, playerY) << " |" << endl;
+    cout << "| " << rooms[0][3]->displayRoom(playerX, playerY) << " | " << rooms[1][3]->displayRoom(playerX, playerY) << " | " << rooms[2][3]->displayRoom(playerX, playerY) << " | " << rooms[3][3]->displayRoom(playerX, playerY) << " |" << endl;
+    cout << "-----------------" << endl;
+
+	cout << rooms[playerX][playerY]->description << endl;
 }
 
-void initialiseMap(Room rooms[4][4])
+void initialiseMap(Room* rooms[4][4])
 {
 	// Manually sets up each room in the 4x4 grid with each room's ways to move. Also allows for changing of classes in code easily if needed
-	rooms[0][0] = Room(0, 0, false, true, false, false); rooms[1][0] = Room(1, 0, false, true, false, false); rooms[2][0] = Room(2, 0, false, true, true, false); rooms[3][0] = Room(3, 0, false, true, false, false);
-	rooms[0][1] = Room(0, 1, true, true, false, false); rooms[1][1] = Room(1, 1, true, true, true, false); rooms[2][1] = Room(2, 1, true, true, false, true); rooms[3][1] = Room(3, 1, true, true, false, false);
-	rooms[0][2] = Room(0, 2, true, true, true, false); rooms[1][2] = Room(1, 2, true, true, false, true); rooms[2][2] = Room(2, 2, true, true, false, false); rooms[3][2] = Room(3, 2, true, true, false, false);
-	rooms[0][3] = Room(0, 3, true, false, false, false); rooms[1][3] = Room(1, 3, true, true, false, false); rooms[2][3] = Room(2, 3, true, false, false, false); rooms[3][3] = Room(3, 3, false, false, false, false);
+	rooms[0][0] = new EmptyRoom(0, 0, false, true, false, false, EmptyRoomDescription); rooms[1][0] = new EmptyRoom(1, 0, false, true, false, false, EmptyRoomDescription); rooms[2][0] = new PuzzleRoom(2, 0, false, true, true, false, EmptyRoomDescription); rooms[3][0] = new EmptyRoom(3, 0, false, true, false, false, EmptyRoomDescription);
+	rooms[0][1] = new EmptyRoom(0, 1, true, true, false, false, EmptyRoomDescription); rooms[1][1] = new PuzzleRoom(1, 1, true, true, true, false, EmptyRoomDescription); rooms[2][1] = new EmptyRoom(2, 1, true, true, false, true, EmptyRoomDescription); rooms[3][1] = new EmptyRoom(3, 1, true, true, false, false, EmptyRoomDescription);
+	rooms[0][2] = new EmptyRoom(0, 2, true, true, true, false, EmptyRoomDescription); rooms[1][2] = new EmptyRoom(1, 2, true, true, false, true, EmptyRoomDescription); rooms[2][2] = new EmptyRoom(2, 2, true, true, false, false, EmptyRoomDescription); rooms[3][2] = new PuzzleRoom(3, 2, true, true, false, false, EmptyRoomDescription);
+	rooms[0][3] = new PuzzleRoom(0, 3, true, false, false, false, PuzzleRoomDescription); rooms[1][3] = new EmptyRoom(1, 3, true, true, false, false, EmptyRoomDescription); rooms[2][3] = new EmptyRoom(2, 3, true, false, false, false, EmptyRoomDescription); rooms[3][3] = new EmptyRoom(3, 3, false, false, false, false, EmptyRoomDescription);
 }
--- Text Based Adventure Assessment/main.cpp
+++ Text Based Adventure Assessment/main.cpp
@@ -1,39 +1,44 @@
 #include "Rooms.h"
 
-Room rooms[4][4];
+
+Room* rooms[4][4];
 int playerX = 0;
 int playerY = 0;
 int main()
 {
-	// initialises the map before the player does anything
-	initialiseMap(rooms);
-	
-	char input;
-	while (true)
-	{
-		// displays the map including the player position (starts at top left)
-		displayMap(rooms, playerX, playerY);
-		
-		// gets player input for movement, works in a way such that only wasd inputs are accepted, and anything else does nothing
-		cout << "Enter command (w/a/s/d to move): ";
-		cin >> input;
-		
-		// wasd movement, also uses the initialised room data to check if the player is allowed to move that way, and if so, does
-		if (input == 'w' && rooms[playerX][playerY].doorNorth) playerY--;
-		else if (input == 's' && rooms[playerX][playerY].doorSouth) playerY++;
-		else if (input == 'a' && rooms[playerX][playerY].doorWest) playerX--;
-		else if (input == 'd' && rooms[playerX][playerY].doorEast) playerX++;
+// initialises the map before the player does anything
+initialiseMap(rooms);
 
-		// if the player is in the bottom right corner of the map
-		if (playerX == 3 && playerY == 3)
-		{
-			// displays where they are on the map one last time, and a message saying they have won
-			displayMap(rooms, playerX, playerY);
-			cout << "You have reached the end of the maze. To play again, please close and reopen the program." << endl;
-			break;
-		}
+char input;
+
+// displays the map including the player position (starts at top left)
+displayMap(rooms, playerX, playerY);
+
+while (true)
+{
+	// gets player input for movement, works in a way such that only wasd inputs are accepted, and anything else does nothing
+	cout << "Enter command (w/a/s/d to move): ";
+	cin >> input;
+	
+	// wasd movement, also uses the initialised room data to check if the player is allowed to move that way, and if so, does
+	if (input == 'w' && rooms[playerX][playerY]->doorNorth) playerY--;
+	else if (input == 's' && rooms[playerX][playerY]->doorSouth) playerY++;
+	else if (input == 'a' && rooms[playerX][playerY]->doorWest) playerX--;
+	else if (input == 'd' && rooms[playerX][playerY]->doorEast) playerX++; 
+
+	displayMap(rooms, playerX, playerY);
+	rooms[playerX][playerY]->specialRoomFunction();
+
+	// if the player is in the bottom right corner of the map
+	if (playerX == 3 && playerY == 3)
+	{
+		// displays where they are on the map one last time, and a message saying they have won
+		displayMap(rooms, playerX, playerY);
+		cout << "You have reached the end of the maze. To play again, please close and reopen the program." << endl;
+		break;
 	}
-	// pauses the program before exiting so the player can see the win message
-	system("pause");
-	return 0;
+}
+// pauses the program before exiting so the player can see the win message
+system("pause");
+return 0;
 }
