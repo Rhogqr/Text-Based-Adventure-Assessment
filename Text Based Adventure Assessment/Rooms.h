98aefa4230ed7d8a455992e5018483e2a4203db2
Rhogar <reuben@coreproductions.com.au>
2025-11-02 15:39:16 +1100

    Movement + Map Display Implemented

--- Text Based Adventure Assessment/Rooms.h
+++ Text Based Adventure Assessment/Rooms.h
@@ -2,3 +2,48 @@
 #include <iostream>
 
 using namespace std;
+
+class Room
+{
+private:
+	int xCoord, yCoord;
+public:
+public:
+	Room()
+	{
+		xCoord = 0;
+		yCoord = 0;
+	}
+ 
+	Room(int x, int y)
+	{
+		xCoord = x;
+		yCoord = y;
+	}
+
+	char displayRoom(int x, int y)
+	{
+		if (x == xCoord && y == yCoord)
+		{
+			return 'X';
+		}
+		else
+		{
+			return ' ';
+		}
+	}
+};
+
+void displayMap(Room rooms[4][4], int playerX, int playerY)
+{
+	for (int i = 0; i < 25; i++)
+	{
+		cout << endl;
+	}
+	cout << "-----------------" << endl;
+	cout << "| " << rooms[0][0].displayRoom(playerX, playerY) << " | " << rooms[1][0].displayRoom(playerX, playerY) << " | " << rooms[2][0].displayRoom(playerX, playerY) << " | " << rooms[3][0].displayRoom(playerX, playerY) << " |" << endl;
+	cout << "| " << rooms[0][1].displayRoom(playerX, playerY) << " | " << rooms[1][1].displayRoom(playerX, playerY) << " | " << rooms[2][1].displayRoom(playerX, playerY) << " | " << rooms[3][1].displayRoom(playerX, playerY) << " |" << endl;
+	cout << "| " << rooms[0][2].displayRoom(playerX, playerY) << " | " << rooms[1][2].displayRoom(playerX, playerY) << " | " << rooms[2][2].displayRoom(playerX, playerY) << " | " << rooms[3][2].displayRoom(playerX, playerY) << " |" << endl;
+	cout << "| " << rooms[0][3].displayRoom(playerX, playerY) << " | " << rooms[1][3].displayRoom(playerX, playerY) << " | " << rooms[2][3].displayRoom(playerX, playerY) << " | " << rooms[3][3].displayRoom(playerX, playerY) << " |" << endl;
+	cout << "-----------------" << endl;
+}
--- Text Based Adventure Assessment/main.cpp
+++ Text Based Adventure Assessment/main.cpp
@@ -1,6 +1,36 @@
 #include "Rooms.h"
 
+Room rooms[4][4];
+int playerX = 0;
+int playerY = 0;
 int main()
 {
-    std::cout << "Hello World!\n";
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++) {
+			rooms[i][j] = Room(i, j);
+		}
+	}
+	char input;
+	while (true)
+	{
+		displayMap(rooms, playerX, playerY);
+		rooms[playerX][playerY].displayRoom(playerX, playerY);
+		
+		cout << "Enter command (w/a/s/d to move): ";
+		cin >> input;
+		
+		if (input == 'w' && playerY > 0) playerY--;
+		else if (input == 's' && playerY < 3) playerY++;
+		else if (input == 'a' && playerX > 0) playerX--;
+		else if (input == 'd' && playerX < 3) playerX++;
+
+		if (playerX == 3 && playerY == 3)
+		{
+			displayMap(rooms, playerX, playerY);
+			cout << "You have reached the end of the maze." << endl;
+			break;
+		}
+	}
+	return 0;
 }
